1856d0139f86421bcd9398f40827607060e0aa7d
Sebastian Moncada <sebastian7584@hotmail.com>
2023-02-02 23:00:20 -0500

    cambio ruta de peticiones al backend4

--- translateProducts/urls.py
+++ translateProducts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import adminProductView, translateProductView
 
 urlpatterns = [
+    path('', adminProductView),
     path('admin', adminProductView),
     path('translate', translateProductView),
 ]
