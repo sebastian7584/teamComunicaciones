ff34a9bca144fd5006be45f45ea270fa12f674be
Sebastian Moncada <sebastian7584@hotmail.com>
2023-02-02 19:42:57 -0500

    cambio de ruta admin a / para pruebas

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/translateProducts/', include('translateProducts.urls')),
     path('api/translateProductsPostpago/', include('translateProductsPostpago.urls'))
