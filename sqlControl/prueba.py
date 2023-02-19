042d752693b2ef1de66664d694dc5d8beeda88b2
Sebastian Moncada <sebastian7584@hotmail.com>
2023-02-19 14:55:15 -0500

    intento arreglo SQL conexion

--- sqlControl/prueba.py
+++ sqlControl/prueba.py
@@ -7,3 +7,12 @@ print(drivers)
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=team.soluciondigital.com.co;DATABASE=Stok;UID=sa;PWD=Soluciondig2015')
 
 cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM dbo.Productos")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
