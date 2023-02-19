470dc50e362043f762a4ec35c049809360923041
Sebastian Moncada <sebastian7584@hotmail.com>
2023-02-19 14:53:17 -0500

    intento arreglo SQL conexion

--- sqlControl/prueba.py
+++ sqlControl/prueba.py
@@ -1,4 +1,9 @@
 import pyodbc
+import pandas as pd
 
 drivers = pyodbc.drivers()
 print(drivers)
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=team.soluciondigital.com.co;DATABASE=Stok;UID=sa;PWD=Soluciondig2015')
+
+cursor = conn.cursor()
